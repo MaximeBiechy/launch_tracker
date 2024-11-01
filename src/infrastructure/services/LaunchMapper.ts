5aea403c7057eb3abe33dc122a860a9ce366a7c7
Maxime Biechy <maxime.biechy5@etu.univ-lorraine.fr>
2024-11-01 18:13:33 +0100

    refactor: Implement LaunchMapper in LaunchApiService to transform API data into Launch entities

---
+++ src/infrastructure/services/LaunchMapper.ts
@@ -0,0 +1,16 @@
+import { Launch } from "../../domain/entities/Launch";
+
+export class LaunchMapper {
+    static toEntity(apiLaunchData: any): Launch {
+        return {
+            id: apiLaunchData.id,
+            name: apiLaunchData.name,
+            date: apiLaunchData.net,
+            image_url: apiLaunchData.image?.image_url || null
+        };
+    }
+
+    static toEntities(apiLaunchDataArray: any[]): Launch[] {
+        return apiLaunchDataArray.map(this.toEntity);
+    }
+}
