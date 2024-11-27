39c4384bbdac3fd7724967d9211a630a04ecf7e6
Maxime Biechy <maxime.biechy5@etu.univ-lorraine.fr>
2024-11-27 20:30:14 +0100

    refactor: update imports

--- frontend/launcher_tracker/src/main.ts
+++ frontend/launcher_tracker/src/main.ts
@@ -1,7 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/router.ts'
-import './assets/input.css'
+import './assets/input.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router/router.ts'; 
 
 const app = createApp(App);
 app.use(router);
