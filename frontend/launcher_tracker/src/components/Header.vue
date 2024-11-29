a8e75aecda8695ff690fc74abf8d3d88748a725a
Maxime Biechy <maxime.biechy5@etu.univ-lorraine.fr>
2024-11-29 14:30:48 +0100

    refactor: Moved component folder into src folder

--- frontend/launcher_tracker/src/components/Header.vue
+++ frontend/launcher_tracker/src/components/Header.vue
@@ -1,32 +1,21 @@
 <template>
-  <header>
+  <header class="flex justify-between items-center h-20 w-full px-20 border-2 border-white">
     <h1 class="text-white text-2xl font-krona">Launch Tracker</h1>
-    <nav class="text-2xl">
+    <nav class="text-2xl flex gap-6">
       <router-link to="/" class="text-white font-krona">Home</router-link>
       <router-link to="/passed-launches" class="text-white font-krona">Passed</router-link>
     </nav>
   </header>
 </template>
 
-<script lang="ts">
-export default {
+<script setup lang="ts">
+import {defineComponent} from "vue";
+
+defineComponent({
   name: 'HeaderComponent',
-};
+});
 </script>
 
 <style>
-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 5rem;
-  width: 100%;
-  padding: 0 2rem;
-  border: 2px solid white;
-}
 
-nav {
-  display: flex;
-  gap: 1rem;
-}
 </style>
